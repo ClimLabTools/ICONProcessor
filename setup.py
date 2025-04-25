7b3fae40c214d6749127f7876ca9902f0baa1905
Alexander Georgi <109804516+geoalxx@users.noreply.github.com>
2025-04-25 23:22:23 +0200

    Updated dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,15 @@ setup(
     name="ICONProcessor",                  # name of your package
     version="0.1",
     packages=find_packages(),             # auto-detect packages
-    install_requires=[],                  # add dependencies here if needed
+    license='MIT',
+    python_requires='>=3.11',
+    install_requires=[
+        "geopandas==1.0.1",
+        "netcdf4==1.7.2",
+        "metpy==1.6.3",
+        "colorama==0.4.6",
+        "suntimes==1.1.2"
+    ],                  # add dependencies here if needed
     description="Utility functions to process ICON grid data and model output data",
     author="Alexander Georgi",
     url="https://github.com/ClimLabTools/ICONProcessor",  # link to repo
